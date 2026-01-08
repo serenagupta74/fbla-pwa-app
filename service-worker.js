686b783394f7844feefc9b765f3d770a1ff432e4
serenagupta74 <serena.gupta74@gmail.com>
2026-01-08 16:55:32 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
+function openTab(evt, tabName) {
+  // Hide all tabcontent
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove active class from all links
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show current tab and add active class
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Open default tab on page load
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementsByClassName("tablink")[0].click();
+});
 
